ecc6a2262b36ef332c66116089196008a59be162
Stefan <xoomer@web.de>
2015-05-22 17:27:28 +0200

    implemented prototype for simple communication between window and connection

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -3,16 +3,37 @@ package control;
 import network.Connection;
 
 public class Controller {
+	public static enum NotificationType {NOTIFY_WAIT, NOTIFY_PLACE, NOTIFY_MOVE,
+										 NOTIFY_YOUR_MOVE, NOTIFY_ILLEGAL, NOTIFY_WIN,
+										 NOTIFY_DISCONNECT, NOTIFY_UNKNOWN}
+	
 	private Connection connection;
+	//private Window window;
 	
 	private String name;
 
 	public Controller() {
-		connection = new Connection();
+		connection = new Connection(this);
+	}
+	
+	public void createWindow() {
+		
 	}
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public void notifyWindow(NotificationType type) {
+		// window.sendNotification(type);
+	}
+	
+	public void sendPlacement(int x, int y) {
+		
+	}
+	
+	public void sendMove(int x, int y) {
+		
 	}
 	
 	public boolean connect(String host, int port) {
--- src/control/Main.java
+++ src/control/Main.java
@@ -1,19 +1,19 @@
 package control;
 
-import view.testClasses.Tmp_testGUI;
-
 public class Main {
 	private static final boolean debug = true;
 	
 	private Controller controller;
-//	private Window window;
 	
 	private void run() {
+		//erzeugt ein Messagedialog und gibt den eingegeben namen zurueck
+		//Tmp_testGUI.first();
+
 		// Create controller
 		controller = new Controller();
-		
-		//erzeugt ein Messagedialog und gibt den eingegeben namen zurueck
-		Tmp_testGUI.first();
+		// der controller sollte wahrscheinlich ganz oben sitzen, dann kann man über den die
+		// notifications zwischen fenster und verbindung schicken
+		controller.createWindow();
 		
 		// Debug code begin -----------------------------------------------------------------------
 		if (debug) {
@@ -21,13 +21,6 @@ public class Main {
 			System.out.println(controller.connect("localhost", 5123));
 		}
 		// Debug code end -------------------------------------------------------------------------
-		
-		// Create window
-//		window = new Window(controller);
-	}
-	
-	public Controller getController() {
-		return controller;
 	}
 	
 	public static void main(String[] args) {
