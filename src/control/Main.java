5a0e8880d385c90d27494dd6c04c763a636e6295
Xoomer93 <xoomer@web.de>
2015-05-27 13:33:24 +0200

    removed Controller class

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,23 +1,11 @@
 package control;
 
-public class Main {
-	private static final boolean debug = true;
-	
-	private Controller controller;
-	
+import network.Connection;
+
+public class Main {	
 	private void run() {
-		// Create controller
-		controller = new Controller();
-		// der controller sollte wahrscheinlich ganz oben sitzen, dann kann man �ber den die
-		// notifications zwischen fenster und verbindung schicken
-		controller.createWindow();
-		
-		// Debug code begin -----------------------------------------------------------------------
-		if (debug) {
-			controller.setName("Test");
-			System.out.println(controller.connect("localhost", 5123));
-		}
-		// Debug code end -------------------------------------------------------------------------
+		Connection connection = new Connection();
+		connection.establishConnection("localhost", 5123);
 	}
 	
 	public static void main(String[] args) {
