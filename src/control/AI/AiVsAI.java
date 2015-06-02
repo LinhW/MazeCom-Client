100b633ef02c0d9e5feb6caab1c3177a56c27907
Linh <m.wuerzburger@fz-juelich.de>
2015-06-02 21:48:58 +0200

    changes for starting multiple games

--- src/control/AI/AiVsAI.java
+++ src/control/AI/AiVsAI.java
@@ -2,16 +2,16 @@ package control.AI;
 
 import java.awt.event.ActionEvent;
 
+import model.jaxb.WinMessageType.Winner;
 import server.Game;
 import server.userInterface.BetterUI;
 import config.Settings;
-import control.AI.ava.Ava;
 import control.network.Connection;
 
 public class AiVsAI {
+	private int p1 = 0, p2 = 0, p3 = 0, p4 = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		AiVsAI a = new AiVsAI();
 		a.start();
 	}
@@ -24,20 +24,37 @@ public class AiVsAI {
 		startClients();
 	}
 
+	public void updateWinnerStat(Winner winner) {
+		switch (winner.getId()) {
+		case 1:
+			p1++;
+			break;
+		case 2:
+			p2++;
+			break;
+		case 3:
+			p3++;
+			break;
+		case 4:
+			p4++;
+			break;
+		}
+	}
+
 	private void startClients() {
-		Connection connection = new Connection();
+		Connection connection = new Connection(this);
 		Client c1 = new Client(new TryAndError(connection), connection);
 		c1.start();
 
-		connection = new Connection();
+		connection = new Connection(this);
 		Client c2 = new Client(new TryAndError(connection), connection);
 		c2.start();
 
-		connection = new Connection();
+		connection = new Connection(this);
 		Client c3 = new Client(new TryAndError(connection), connection);
 		c3.start();
 
-		connection = new Connection();
+		connection = new Connection(this);
 		Client c4 = new Client(new TryAndError(connection), connection);
 		c4.start();
 	}
@@ -49,7 +66,7 @@ public class AiVsAI {
 		@Override
 		public void start() {
 			con.setPlayer(player);
-			con.establishConnection("localhost", 5123);
+			con.establishConnection("localhost", Settings.PORT);
 		}
 
 		public Client(Player player, Connection connection) {
