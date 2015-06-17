7795a17204d9329e681f43718ebd8c5a36e1a5ac
Xoomer93 <xoomer@web.de>
2015-06-17 13:16:42 +0200

    changed network clases

--- src/control/network/UTFInputStream.java
+++ src/control/network/UTFInputStream.java
@@ -1,49 +1,48 @@
 package control.network;
+
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
- 
-class UTFInputStream {
- 
-  private final InputStream is;
- 
-  UTFInputStream(InputStream stream) {
-    this.is = stream;
-  }
- 
-  String readUTF8() throws IOException {
-    byte[] tmp = this.readNBytes(4);
-    int len = 0;
-    len |= (tmp[3] & 0xff);
-    len <<= 8;
-    len |= (tmp[2] & 0xff);
-    len <<= 8;
-    len |= (tmp[1] & 0xff);
-    len <<= 8;
-    len |= (tmp[0] & 0xff);
-    byte[] bytes = this.readNBytes(len);
-    String message = new String(bytes, "UTF-8");
-    return message;
-  }
- 
-//  public void close() throws IOException {
-//    this.is.close();
-//  }
- 
-  private byte[] readNBytes(int n) throws IOException {
-    if (n <= 0) {
-      throw new IllegalArgumentException();
-    }
-    byte buf[] = new byte[n];
-    int readcount = 0;
-    int lastreadcount;
-    while (readcount < n) {
-      lastreadcount = this.is.read(buf, readcount, n - readcount);
-      if (lastreadcount == -1) {
-        throw new EOFException(String.format("Could only get %d of %d requested bytes", readcount, n));
-      }
-      readcount += lastreadcount;
-    }
-    return buf;
-  }
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class UTFInputStream {
+
+	private InputStream is;
+
+	public UTFInputStream(InputStream stream) {
+		this.is = stream;
+	}
+
+	public String readUTF8() throws IOException {
+		ByteBuffer bf = ByteBuffer.wrap(this.readNBytes(4));
+		bf.order(ByteOrder.BIG_ENDIAN); // Java always use hostorder. See javadoc
+		byte[] bytes = this.readNBytes(bf.getInt(0));
+		return new String(bytes, "UTF-8"); //$NON-NLS-1$
+	}
+
+	public void close() throws IOException {
+		this.is.close();
+	}
+
+	private byte[] readNBytes(int n) throws IOException {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		byte buf[] = new byte[n];
+		int readcount = 0;
+		int lastreadcount;
+		while (readcount < n) {
+			lastreadcount = this.is.read(buf, readcount, n - readcount);
+			if (lastreadcount == -1) {
+				throw new EOFException(String.format(
+						Messages.getString("UTFInputStream.EOFException"), //$NON-NLS-1$
+						readcount, n));
+			}
+			readcount += lastreadcount;
+
+		}
+
+		return buf;
+	}
 }
--- src/control/network/UTFOutputStream.java
+++ src/control/network/UTFOutputStream.java
@@ -2,38 +2,31 @@ package control.network;
 
 import java.io.IOException;
 import java.io.OutputStream;
- 
-class UTFOutputStream {
- 
-  private final OutputStream os;
- 
-  UTFOutputStream(OutputStream stream) {
-    os = stream;
-  }
- 
-  void writeUTF8(String text) throws IOException
-  {
-    byte[] bytes = text.getBytes("UTF-8");
-    int len=bytes.length;
- 
-    byte[] lenbuf = new byte[4];
-    lenbuf[0] = (byte) (len & 0xff);
-    len >>= 8;
-    lenbuf[1] = (byte) (len & 0xff);
-    len >>= 8;
-    lenbuf[2] = (byte) (len & 0xff);
-    len >>= 8;
-    lenbuf[3] = (byte) (len & 0xff);
- 
-    os.write(lenbuf);
-    os.write(bytes);
-  }
- 
-//  public void flush() throws IOException {
-//    os.flush();
-//  }
-// 
-//  public void close() throws IOException {
-//    this.os.close();
-//  }
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class UTFOutputStream {
+
+	private OutputStream os;
+
+	public UTFOutputStream(OutputStream stream) {
+		os = stream;
+	}
+
+	public void writeUTF8(String text) throws IOException {
+		byte[] bytes = text.getBytes("UTF-8"); //$NON-NLS-1$
+		ByteBuffer bf = ByteBuffer.allocate(4);
+		bf.order(ByteOrder.BIG_ENDIAN); // Transform to network order
+		bf.putInt(bytes.length);
+		os.write(bf.array());
+		os.write(bytes);
+	}
+
+	public void flush() throws IOException {
+		os.flush();
+	}
+
+	public void close() throws IOException {
+		this.os.close();
+	}
 }
